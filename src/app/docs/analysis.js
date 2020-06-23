a413f1a920926ce1ca6455e1170bc678b1bfd88f
Stephen Chang <schang@thezebra.com>
2020-06-23 13:44:16 -0500

    Overview header fix and removal of getParents and getReferences in all docs files

--- src/app/docs/analysis.js
+++ src/app/docs/analysis.js
@@ -10,32 +10,6 @@ angular
 .controller('AnalysisCtrl', ['$scope', '$state', 'project', 'code', '$transitions', '$anchorScroll', '$location',
             function($scope, $state, projectService, codeService, $transitions, $anchorScroll, $location) {
 
-    function getReferences(project, macro) {
-        var references = _.filter(project.nodes, function(node) {
-            if (node.depends_on && node.depends_on.nodes && node.depends_on.nodes.length) {
-                if (_.contains(node.depends_on.nodes, macro.unique_id)) {
-                    return true;
-                }
-            }
-            return false;
-        });
-
-        // TODO : include macros?
-        return _.groupBy(references, 'resource_type');
-    }
-
-    function getParents (project, model) {
-        var parents = _.filter(project.nodes, function(node) {
-            if (model.depends_on && model.depends_on.nodes && model.depends_on.nodes.length) {
-                if (_.contains(model.depends_on.nodes, node.unique_id)) {
-                    return true;
-                }
-            }
-            return false;
-        });
-        
-        return _.groupBy(parents, 'resource_type');
-    }
     $scope.model_uid = $state.params.unique_id;
     $scope.project = projectService;
     $scope.codeService = codeService;
--- src/app/docs/test.js
+++ src/app/docs/test.js
@@ -10,33 +10,6 @@ angular
 .controller('TestCtrl', ['$scope', '$state', 'project', 'code', '$anchorScroll', '$location',
             function($scope, $state, projectService, codeService, $anchorScroll, $location) {
 
-    function getReferences(project, macro) {
-        var references = _.filter(project.nodes, function(node) {
-            if (node.depends_on && node.depends_on.nodes && node.depends_on.nodes.length) {
-                if (_.contains(node.depends_on.nodes, macro.unique_id)) {
-                    return true;
-                }
-            }
-            return false;
-        });
-
-        // TODO : include macros?
-        return _.groupBy(references, 'resource_type');
-    }
-
-    function getParents (project, model) {
-        var parents = _.filter(project.nodes, function(node) {
-            if (model.depends_on && model.depends_on.nodes && model.depends_on.nodes.length) {
-                if (_.contains(model.depends_on.nodes, node.unique_id)) {
-                    return true;
-                }
-            }
-            return false;
-        });
-        
-        return _.groupBy(parents, 'resource_type');
-    }
-
     $scope.model_uid = $state.params.unique_id;
     $scope.tab = $state.params.tab;
     $scope.project = projectService;
