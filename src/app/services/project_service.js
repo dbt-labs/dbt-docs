b01b17d47a6484e8ff403ead77b32021edd83ac9
Joel Labes <joel.labes@educationperfect.com>
2021-01-04 19:49:02 +1300

    Add rest of transformed node

--- src/app/services/project_service.js
+++ src/app/services/project_service.js
@@ -318,12 +318,16 @@ angular
     function prepareModelsForSearching(models) {
     	var transformed = [];
     	for (var key in models) {
-    		transformed.push(
-    			{
+    		var model = models[key];
+    		newNode = {
     				"node": key,
-    				"alias": models[key].alias,
+    				"alias": model.alias,
+    				"description": model.description
+    				"columns": model.columns.map(c => {c.name, c.description, c.tags}),
+    				"tags": model.tags,
+    				"raw_sql": model.raw_sql
     			}
-    		)
+    		transformed.push(newNode);
     	}
     	console.log(transformed);
     }
