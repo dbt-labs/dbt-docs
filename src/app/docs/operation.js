58a2d5af67d06fa60c8e317768e66b6fdb3a8ce2
Emily Rockman <emily.rockman@dbtlabs.com>
2022-02-24 14:40:47 -0600

    DAG fails to render if operation exists (#248)
    * first pass at resolving errors.
    
    * remove operation from DAG, add comment, operation still shows on detail pages
    
    * add page to display operation details so we can link somewhere
    
    * add changelog
    
    * fix indentation

---
+++ src/app/docs/operation.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const angular = require('angular');
+const dag_utils = require('./dag_utils')
+require("./styles.css");
+
+angular
+.module('dbt')
+.controller('OperationCtrl', ['$scope', '$state', 'project', 'code', '$anchorScroll', '$location',
+            function($scope, $state, projectService, codeService, $anchorScroll, $location) {
+
+    $scope.model_uid = $state.params.unique_id;
+    $scope.tab = $state.params.tab;
+    $scope.project = projectService;
+    $scope.codeService = codeService;
+
+    $scope.versions = {}
+
+    $scope.model = {};
+    projectService.ready(function(project) {
+        let mod = project.nodes[$scope.model_uid];
+        $scope.model = mod;
+        $scope.references = dag_utils.getReferences(project, mod);
+        $scope.referencesLength = Object.keys($scope.references).length;
+        $scope.parents = dag_utils.getParents(project, mod);
+        $scope.parentsLength = Object.keys($scope.parents).length;
+
+        var default_compiled = '\n-- compiled SQL not found for this model\n';
+        $scope.versions = {
+            'Source': $scope.model.raw_sql,
+            'Compiled': $scope.model.compiled_sql || default_compiled
+        }
+
+        setTimeout(function() {
+            $anchorScroll();
+        }, 0);
+    })
+}]);
