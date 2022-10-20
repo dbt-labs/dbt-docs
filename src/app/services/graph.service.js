a00489951e970c9a74e235c12c231ab51ea905bc
Paul Schmidt <paul.schmidt@homechef.com>
2022-10-20 13:47:30 -0500

    add selector for python models (#324)
    * squash all commits to single
    
    * update color to #6A5ACD

--- src/app/services/graph.service.js
+++ src/app/services/graph.service.js
@@ -193,9 +193,15 @@ angular
                     }
                 },
                 {
-                    selector: 'node[node_color]', 
+                    selector: 'node[language="python"]',
                     style: {
-                        'background-color': 'data(node_color)', 
+                        'background-color': '#6a5acd',
+                    }
+                },
+                {
+                    selector: 'node[node_color]',
+                    style: {
+                        'background-color': 'data(node_color)',
                     }
                 },
                 {
