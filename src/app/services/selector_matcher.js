4d54e48cfc65bcaaf229ed132c9cc5e959c36875
Emily Rockman <emily.rockman@dbtlabs.com>
2023-03-07 18:05:18 -0600

    first pass at adding group selector

--- src/app/services/selector_matcher.js
+++ src/app/services/selector_matcher.js
@@ -9,6 +9,7 @@ var SELECTOR_TYPE = {
     SOURCE: 'source',
     EXPOSURE: 'exposure',
     METRIC: 'metric',
+    GROUP: 'group',
     PATH: 'path',
     FILE: 'file',
     PACKAGE: 'package',
@@ -24,6 +25,7 @@ NODE_MATCHERS[SELECTOR_TYPE.TAG] = getNodesByTag;
 NODE_MATCHERS[SELECTOR_TYPE.SOURCE] = getNodesBySource;
 NODE_MATCHERS[SELECTOR_TYPE.EXPOSURE] = getNodesByExposure;
 NODE_MATCHERS[SELECTOR_TYPE.METRIC] = getNodesByMetric;
+NODE_MATCHERS[SELECTOR_TYPE.GROUP] = getNodesByGroup;
 NODE_MATCHERS[SELECTOR_TYPE.PATH] = getNodesByPath;
 NODE_MATCHERS[SELECTOR_TYPE.FILE] = getNodesByFile;
 NODE_MATCHERS[SELECTOR_TYPE.PACKAGE] = getNodesByPackage;
@@ -298,6 +300,33 @@ function getNodesByMetric(elements, metric) {
         }
     })
     return nodes;
+}
+
+function getNodesByGroup(elements, group) {
+    var nodes = [];
+    _.each(elements, function(node_obj) {
+        var node = node_obj.data;
+
+        if (node.group == group) {
+            nodes.push(node);
+        } else {
+            console.log(node);
+        }
+    });
+    return nodes;
+    // _.each(nodes, function(node) {
+    //     const show = _.get(node, ['docs', 'show'], true);
+    //     const excludeNodes = ['source', 'exposure', 'seed', 'macro']
+    //     if (node.resource_type in excludeNodes || !show || node.access === "private") {
+    //         return;
+    //     }
+
+    //     var name = node.name;
+    //     var name = node.access === "protected" ? `${node.name} (protected)` : node.name;
+
+    //     var group = node.group;
+
+
 }
 
 function getNodesFromSpec(dag, pristine_nodes, maxHops, selector) {
