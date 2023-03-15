7e2871a441df375004ebe7e78a31564a8769f622
Jeremy Cohen <jeremy@dbtlabs.com>
2023-03-15 12:46:51 +0100

    Take two: distinguish nodes in DAG viz based on `access` (#398)
    * Update display options for node access
    
    * Update CI project
    
    * Update changie entry
    
    * Rm visual distinction bw protected + public nodes
    
    * Add some custom node_colors to ci-project for fun

--- src/app/services/graph.service.js
+++ src/app/services/graph.service.js
@@ -137,6 +137,7 @@ angular
                     style: {
                         'text-margin-x': '5px',
                         'background-color': '#0094b3',
+                        'border-color': '#0094b3',
                         'font-size': '16px',
                         'shape': 'ellipse',
                         'color': '#fff',
@@ -153,6 +154,7 @@ angular
                     selector: 'node.horizontal',
                     style: {
                         'background-color': '#0094b3',
+                        'border-color': '#0094b3',
                         'font-size': '24px',
                         'shape': 'roundrectangle',
                         'color': '#fff',
@@ -178,67 +180,72 @@ angular
                     selector: 'node[resource_type="source"]',
                     style: {
                         'background-color': '#5fb825',
+                        'border-color': '#5fb825',
                     }
                 },
                 {
                     selector: 'node[resource_type="exposure"]',
                     style: {
                         'background-color': '#ff694b',
+                        'border-color': '#ff694b',
                     }
                 },
                 {
                     selector: 'node[resource_type="metric"]',
                     style: {
                         'background-color': '#ff5688',
+                        'border-color': '#ff5688',
                     }
                 },
                 {
                     selector: 'node[language="python"]',
                     style: {
                         'background-color': '#6a5acd',
+                        'border-color': '#6a5acd',
                     }
                 },
                 {
                     selector: 'node[node_color]',
                     style: {
                         'background-color': 'data(node_color)',
+                        'border-color': 'data(node_color)',
                     }
                 },
                 {
                     selector: 'node[selected=1]',
                     style: {
                         'background-color': '#bd6bb6',
+                        'border-color': '#bd6bb6',
                     }
                 },
                 {
                     selector: 'node.horizontal[selected=1]',
                     style: {
-                        'background-color': '#88447d'
+                        'background-color': '#88447d',
+                        'border-color': '#88447d',
                     }
                 },
                 {
                     selector: 'node.horizontal.dirty',
                     style: {
                         'background-color': '#919599',
+                        'border-color': '#919599',
                     }
                 },
                 {
                     selector: 'node[hidden=1]',
                     style: {
                         'background-color': '#919599',
+                        'border-color': '#919599',
                         'background-opacity': 0.5,
                     }
                 },
                 {
                     selector: 'node[access="private"]',
                     style: {
-                        'shape': 'rectangle',
-                    }
-                },
-                {
-                    selector: 'node[access="public"]',
-                    style: {
-                        'shape': 'ellipse',
+                        'background-opacity': 0.2,
+                        'border-width': 2,
+                        'ghost': 'no',
                     }
                 },
             ],
