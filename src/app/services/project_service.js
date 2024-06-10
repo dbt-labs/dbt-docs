8f45de740baf9f0f2d0f5154faba6960da2450a1
sercancicek <sercancicek@gmail.com>
2024-06-10 11:12:03 +0200

    fix: restructure the required changes among files

--- src/app/services/project_service.js
+++ src/app/services/project_service.js
@@ -3,6 +3,7 @@ const angular = require('angular');
 const $ = require('jquery');
 const _ = require('lodash');
 const { getQuoteChar } = require('./compat');
+const { assignSearchRelevance } = require('./project_service_utils');
 
 import merge from 'deepmerge';
 
@@ -357,7 +358,7 @@ angular
     }
 
     service.search = function(q) {
-        if (q.length == 0) {
+        if (q.length === 0) {
             return _.map(service.project.searchable, function(model) {
                 return {
                     model: model,
@@ -376,7 +377,7 @@ angular
                 });
             }
         });
-        return res;
+        return assignSearchRelevance(res, q);
     }
 
     function clean_project_macros(macros, adapter) {
