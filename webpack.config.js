edd7bdc200735f538428dff0ede9f951369478f7
Kshitij Aranke <kshitij.aranke@dbtlabs.com>
2024-06-13 14:44:05 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
+const HtmlInlineScriptPlugin = require("html-inline-script-webpack-plugin");
 
 const BUILD_ENV = process.env.DBT_DOCS_ENV || "production";
 
